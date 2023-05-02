d17d5d79bdf3b295be37e97587230b757df7137c
AitziberU <132055247+AitziberU@users.noreply.github.com>
2023-05-02 14:51:48 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,21 @@
+let Soinu_limitea = 125
+basic.forever(function () {
+    if (input.soundLevel() < Soinu_limitea) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+    basic.pause(500)
+})
